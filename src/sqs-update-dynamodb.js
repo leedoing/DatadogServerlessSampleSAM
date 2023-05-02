a069e031689a5dc37309bd06788ebf77701db9aa
leedoing <lluckyy77@gmail.com>
2023-05-02 12:45:41 +0900

    -

--- src/sqs-update-dynamodb.js
+++ src/sqs-update-dynamodb.js
@@ -4,12 +4,12 @@ const { DDB_VOTES_TABLE_NAME } = process.env;
 
 const UpdateDynamoDB = async function (body) {
   try {
-    for (const key in body.survey_name) {
+    for (const key in body.vote_name) {
       await ddb
         .update({
           TableName: DDB_VOTES_TABLE_NAME,
           Key: {
-            survey_name: body.survey_name[key],
+            vote_name: body.vote_name[key],
           },
           UpdateExpression: `SET votes = if_not_exists(votes, :default_votes) + :value`,
           ExpressionAttributeValues: {
